2a957adf48b7d71dd3068e3bbf2d50293ea5bd98
pdmdev <danish.m.imtiaz@gmail.com>
2016-07-26 16:32:34 -0500

    animation

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives', 'stripe', 'ngOpenFB', 'angular-storage', 'chart.js','googleplus'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives', 'stripe', 'ngOpenFB', 'angular-storage', 'chart.js','googleplus', 'ngAnimate'])
 
   .run(function($ionicPlatform, ngFB) {
 
