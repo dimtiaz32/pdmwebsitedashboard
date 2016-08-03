b7cff7dbef446fe220d12fd145b27a73944b06da
pdmdev <danish.m.imtiaz@gmail.com>
2016-08-03 11:56:41 -0500

    controller for anglar chips

--- www/js/controllers/raceController.js
+++ www/js/controllers/raceController.js
@@ -39,7 +39,94 @@ angular.module('starter.raceController', ['starter.appServices',
   })
 
   .controller('FindRacesCtrl', function($scope) {
+    /*Chips Stuff
+     var self = this;
+     this.readonly = false;
+     this.match = true;
+     this.tags = [];
+     this.cities = ['Chicago', 'Baltimore', 'Arlington'];
+     this.distances = ['5k', '10k', '15k'];
+     this.possibleCities = ['Washington', 'Duluth', 'New York', 'Boston'];
+     this.possibleDistances = ['20k', '25k', '30k'];
+     this.newChip= function(chip) {
+     return chip;
+     };
+     */
 
+    /*Contact Chips -- Cities*/
+    this.cityQuerySearch = function(query) {
+      var results = query ? this.allCities.filter(createFilterForCities(query)) : [];
+      return results;
+    };
+
+    this.allCities = loadCities();
+    this.cities = [this.allCities[0]];
+    this.filterSelected = true;
+
+
+    function createFilterForCities(query) {
+      var lowercaseQuery = angular.lowercase(query);
+      return function filterFn(city) {
+        return (city.lowercaseName.indexOf(lowercaseQuery) != -1);
+      };
+    }
+
+    function loadCities() {
+      var cities = [
+        'New York',
+        'Boston',
+        'Arlington',
+        'Washington',
+        'Baltimore',
+        'Duluth',
+        'Montpelier',
+        'Richmond',
+        'Dover'
+      ];
+      return cities.map(function (c, index) {
+        var cParts = c.split(' ');
+        var city = {
+          name: c,
+        };
+        city.lowercaseName = city.name.toLowerCase();
+        return city;
+      });
+    }
+    /*Contact Chips -- distances*/
+    this.distanceQuerySearch = function (query) {
+      var results = query ? this.allDistances.filter(createFilterForDistances(query)) : [];
+      return results;
+    };
+
+    this.allDistances = loadDistances();
+    this.distances = [this.allDistances[0]];
+
+    function createFilterForDistances(query) {
+      var lowercaseQuery = angular.lowercase(query);
+      return function filterFn(distance) {
+        return (distance.lowercaseName.indexOf(lowercaseQuery) != -1);
+      };
+    }
+
+    function loadDistances() {
+      var distances = [
+        '5k',
+        '10k',
+        '15k',
+        '20k',
+        '25k'
+      ];
+      return distances.map(function (d, index) {
+        var dParts = d.split(' ');
+        var distance = {
+          name: d,
+        };
+        distance.lowercaseName = distance.name.toLowerCase();
+        return distance;
+      });
+    }
+
+    /*End Chips Stuff*/
   })
 
   .controller('PastRacesCtrl', function($scope) {
