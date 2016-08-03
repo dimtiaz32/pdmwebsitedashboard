0cb53450e767e8b0ec4db86b8b94fcfb27e689d2
pdmdev <danish.m.imtiaz@gmail.com>
2016-08-03 11:39:11 -0500

    angular chips

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives', 'stripe', 'ngOpenFB', 'angular-storage', 'chart.js','googleplus','ngCookies', 'angular-svg-round-progressbar'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives', 'stripe', 'ngOpenFB', 'angular-storage', 'chart.js','googleplus','ngCookies', 'angular-svg-round-progressbar', 'ngMaterial'])
 
   .run(function($ionicPlatform, ngFB) {
 
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,7 +7,7 @@ angular.module('starter.controllers', ['starter.appServices',
 
     'starter.historyServices',
 
-    'starter.runServices','ionic','ngCordova','ngOpenFB', 'chart.js','ngCookies', 'ionic.contrib.drawer.vertical', 'angular-svg-round-progressbar'])
+    'starter.runServices','ionic','ngCordova','ngOpenFB', 'chart.js','ngCookies', 'ionic.contrib.drawer.vertical', 'angular-svg-round-progressbar', 'ngMaterial'])
 
 
   .controller('SignUpCtrl', function($scope, $rootScope, $ionicModal, $timeout, AuthAPI, $window, UserAPI){
@@ -3978,6 +3978,94 @@ angular.module('starter.controllers', ['starter.appServices',
   })
 
   .controller('FindRacesCtrl', function($scope) {
+    /*Chips Stuff
+    var self = this;
+    this.readonly = false;
+    this.match = true;
+    this.tags = [];
+    this.cities = ['Chicago', 'Baltimore', 'Arlington'];
+    this.distances = ['5k', '10k', '15k'];
+    this.possibleCities = ['Washington', 'Duluth', 'New York', 'Boston'];
+    this.possibleDistances = ['20k', '25k', '30k'];
+    this.newChip= function(chip) {
+      return chip;
+    };
+    */
+
+    /*Contact Chips -- Cities*/
+    this.cityQuerySearch = function(query) {
+      var results = query ? this.allCities.filter(createFilterForCities(query)) : [];
+      return results;
+    };
+
+    this.allCities = loadCities();
+    this.cities = [this.allCities[0]];
+    this.filterSelected = true;
+
+
+    function createFilterForCities(query) {
+      var lowercaseQuery = angular.lowercase(query);
+      return function filterFn(city) {
+        return (city.lowercaseName.indexOf(lowercaseQuery) != -1);
+      };
+    }
+
+    function loadCities() {
+      var cities = [
+        'New York',
+        'Boston',
+        'Arlington',
+        'Washington',
+        'Baltimore',
+        'Duluth',
+        'Montpelier',
+        'Richmond',
+        'Dover'
+      ];
+      return cities.map(function (c, index) {
+        var cParts = c.split(' ');
+        var city = {
+          name: c,
+        };
+        city.lowercaseName = city.name.toLowerCase();
+        return city;
+      });
+    }
+    /*Contact Chips -- distances*/
+    this.distanceQuerySearch = function (query) {
+      var results = query ? this.allDistances.filter(createFilterForDistances(query)) : [];
+      return results;
+    };
+
+    this.allDistances = loadDistances();
+    this.distances = [this.allDistances[0]];
+
+    function createFilterForDistances(query) {
+      var lowercaseQuery = angular.lowercase(query);
+      return function filterFn(distance) {
+        return (distance.lowercaseName.indexOf(lowercaseQuery) != -1);
+      };
+    }
+
+    function loadDistances() {
+      var distances = [
+        '5k',
+        '10k',
+        '15k',
+        '20k',
+        '25k'
+      ];
+      return distances.map(function (d, index) {
+        var dParts = d.split(' ');
+        var distance = {
+          name: d,
+        };
+        distance.lowercaseName = distance.name.toLowerCase();
+        return distance;
+      });
+    }
+
+    /*End Chips Stuff*/
 
   })
 
@@ -3985,3 +4073,4 @@ angular.module('starter.controllers', ['starter.appServices',
 
 
   });
+
