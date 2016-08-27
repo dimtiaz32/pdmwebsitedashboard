dac6f360e57fc0d739ae98e57e7a9105f499603d
pdmdev <danish.m.imtiaz@gmail.com>
2016-08-27 19:31:23 -0400

    clear login and password after logout

--- www/js/controllers/authController.js
+++ www/js/controllers/authController.js
@@ -214,6 +214,7 @@ angular.module('starter.authController', ['starter.appServices',
 
     };
 
+
     $scope.setUserCharity  = function(charityId){
       console.log('setUserCharityCalled with ID: ' + charityId);
       CharityAPI.getById(charityId)
@@ -318,6 +319,10 @@ angular.module('starter.authController', ['starter.appServices',
 
           // $rootScope.$broadcast("initial");
           // $rootScope.$broadcast('newMap');
+
+          //Set these inputs to be empty
+          $scope.user.email = '';
+          $scope.user.password = '';
 
           $rootScope.$broadcast('LoadRun');
           $rootScope.$broadcast('ChangeCharity');
